f78544e2347e13a6431b5d7073f6c0b387133082
joao barbirato <joaobarbirato@gmail.com>
2018-10-09 10:50:13 -0300

    Generate children added; two-cities way fully functional

--- TrajetoDuasCidades.R
+++ TrajetoDuasCidades.R
@@ -1,53 +1,61 @@
-source("Trajeto.R")
+source("Estado.R")
 
-Trajeto <- function(desc=NULL, pai=NULL, cidades=NULL){
+Trajeto <- function(desc=NULL, pai=NULL, cidades=NULL, fun_heuristica=NULL){
   
-  e <- environment()
-  
-  assign("desc", desc, envir = e)
-  assign("pai", pai, envir = e)
-  assign("cidades", cidades, envir = e)
-  assign("g", 0, envir = e)
-  assign("h", Inf, envir = e)
-  assign("f", Inf, envir = e)
-  
-  class(e) <- c("Trajeto", "Estado")
-  
-  return(e)
+	e <- environment()
+	
+	assign("desc", desc, envir = e)
+	assign("pai", pai, envir = e)
+	assign("cidades", cidades, envir = e)
+	assign("fun_h", fun_heuristica, envir = e)
+	assign("g", 0, envir = e)
+	assign("h", Inf, envir = e)
+	assign("f", Inf, envir = e)
+	
+	class(e) <- c("Trajeto", "Estado")
+	
+	return(e)
 }
 
 ## Sobrecarregando o operador "==" para comparação entre Trajetos
 Ops.Trajeto = function(obj1,obj2){
-  if(.Generic == "=="){
-    return(all(obj1$desc == obj2$desc))
-  }
+ 	if(.Generic == "=="){
+  		return(all(obj1$desc == obj2$desc))
+ 	}
 }
 
 ## Sobrecarga da função genérica print
 print.Trajeto <- function(obj){
-  cat(obj$desc, "\n")
-  cat(obj$g, "\n")
-  cat(obj$h, "\n")
+	cat("CIDADE: ", obj$desc, "\n")
+	cat("G(N): ", obj$g, "\n")
+	cat("H(N): ", obj$h, "\n")
+	cat("F(N): ", obj$f, "\n")
 }
 
 ## Criação do método genérico "heuristica"
 heuristica <- function(atual, ...) {
-  UseMethod("heuristica")
-}
-
-## Função padrão para o método genérico "heuristica"
-## Deve ser implementada para o problema específico
-heuristica.default <- function(atual, ...) {
-  print("Funcao Generica. Defina a heuristica para o seu problema!\n")
-  return(NULL)
+    return(atual$fun_h()[1,atual$desc])
 }
 
 ## Criação do método genérico "geraFilhos"
 geraFilhos <- function(obj) {
-  UseMethod("geraFilhos")
-}
+    filhosDesc <- list()
+	filhos <- list()
 
-geraFilhos.default <- function(obj) {
-  print("Funcao Generica. Defina a geração de filhos para o seu problema!\n")
-  return(NULL)
+	cidades <- obj$cidades
+	vizinhos <- t(cidades[obj$desc,])
+	colnames(vizinhos, do.NULL=FALSE)
+	colnames(vizinhos) <- colnames(cidades)
+	filhosDesc <- c(
+		filhosDesc,
+		colnames(vizinhos)[apply(vizinhos,1,function(vizinhos){which(vizinhos != 0, arr.ind=T)})]
+	)
+	for(filhoDesc in filhosDesc){
+		filho <- Trajeto(desc=filhoDesc, pai=obj, fun_h=obj$fun_heuristica, cidades=obj$cidades)
+		filho$h <- heuristica(filho)
+		filho$g <- obj$g + vizinhos[1,filhoDesc]
+		filho$f <- filho$h + filho$g
+		filhos <- c(filhos, list(filho))
+	}
+	return(filhos)
 }
--- exemploTrajeto.R
+++ exemploTrajeto.R
@@ -1,10 +1,18 @@
+CITY_NAMES <- c(
+    "A", "B", "C", "D",
+    "F", "G", "L", "M",
+    "O", "P", "R", "S",
+    "T", "U", "Z"
+)
+
+euclideandistance.get <- function(){
+    distances <- t(c(366,0,160,242,178,77,244,241,380,98,193,253,329,80,374))
+    colnames(distances, do.NULL=FALSE)
+    colnames(distances) = CITY_NAMES
+    return(distances)
+}
+
 transitionmatrix.get <- function() {
-    names <- c(
-        "A", "B", "C", "D",
-        "F", "G", "L", "M",
-        "O", "P", "R", "S",
-        "T", "U", "Z"
-    )
     mat <- matrix(c(
         0,0,0,0,0,0,0,0,0,0,0,140,118,0,75
         ,0,0,0,0,211,90,0,0,0,101,0,0,0,85,0
@@ -24,20 +32,20 @@ transitionmatrix.get <- function() {
     ), nrow=15, ncol=15)
     rownames(mat, do.NULL = FALSE)
     colnames(mat, do.NULL = FALSE)
-    rownames(mat) <- names
-    colnames(mat) <- names
-
+    rownames(mat) <- CITY_NAMES
+    colnames(mat) <- CITY_NAMES
     return(mat)
 }
 
 transitionmatrix <- transitionmatrix.get()
+ed <- euclideandistance.get
 
 source("TrajetoDuasCidades.R")
 source("buscaDesinformada.R")
 source("buscaInformada.R")
 
-inicial <- Trajeto(desc = "A", cidades = transitionmatrix)
-objetivo <- Trajeto()
+inicial <- Trajeto(desc = "A", cidades = transitionmatrix, fun_heuristica = euclideandistance.get)
+objetivo <- Trajeto(cidades = transitionmatrix, fun_heuristica = euclideandistance.get)
 objetivo$desc <- "U"
 
 cat("====\tBusca em Largura\t====\n")
